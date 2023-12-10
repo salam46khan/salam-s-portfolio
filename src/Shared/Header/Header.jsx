ef7d6a8bc4234d03d0a9340c9e8c1495968b7106
salam46khan <salam46.khan@gmail.com>
2023-12-11 04:18:03 +0600

    add header section

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,28 +1,46 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from '../../../public/logo.png'
+import { FaBars } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import { useState } from "react";
 
 const Header = () => {
+    const [navopen, setNavopen] = useState(false);
+
+    const navItem = <>
+        <li><NavLink to={'/'}>Home</NavLink></li>
+        <li><NavLink to={'/about'}>About</NavLink></li>
+        <li><a>Link</a></li>
+        
+        
+
+    </>
     return (
-        <div>
-            <div className="navbar bg-base-100">
-                <div className="bar">
-dd
-                </div>
+        <div className="bg-violet-800 h-[] fixed top-0 left-0 w-full ">
+            <div className="navbar container mx-auto relative flex justify-between items-center">
+
                 <div className="logo">
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <Link to={'/'}>
+                        <img className="h-14 md:h-16  rounded-full" src={logo} alt="" />
+                    </Link>
                 </div>
-                <div className="nav">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Link</a></li>
-                        <li>
-                            <details>
-                                <summary>
-                                    Parent
-                                </summary>
-                                <ul className="p-2 bg-base-100 rounded-t-none">
-                                    <li><a>Link 1</a></li>
-                                    <li><a>Link 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
+
+                <div className="bar text-white text-2xl md:hidden block ml-auto">
+                    <button onClick={() => setNavopen(!navopen)}>
+                        
+                        {
+                            navopen ?  <ImCross /> : <FaBars />
+                        }
+                    </button>
+                </div>
+                <div className="md-nav hidden md:block text-white ml-auto ">
+                    <ul className="menu menu-horizontal text-xl px-1">
+                        {navItem}
+                    </ul>
+                </div>
+                <div className={`${navopen ? 'left-0' : 'left-[140%]'} sm-nav block md:hidden  absolute w-full top-[62px] bg-opacity-80 duration-300 text-white bg-violet-800 `}>
+                    <ul className="menu menu-vertical text-xl px-1">
+                        {navItem}
                     </ul>
                 </div>
             </div>
